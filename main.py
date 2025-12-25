665865551830dff214baf6cee26abdd6e5ec9d33
Arslan-Iumaguzhin <arsiktraining@gmail.com>
2025-12-25 17:13:26 +0300

    added last string where it was neccesary, added project description in README.md, new title of project

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ from http import HTTPStatus
 from fastapi.responses import HTMLResponse
 
 
-app = FastAPI(title="My service", version = "0.1.0")
+app = FastAPI(title="todoshka", version = "0.1.0")
 
 @app.get("/ping")
 def ping():
     return Response(content = json.dumps({"ping":"pong!"}), status_code = HTTPStatus.OK)
-
-@app.get("/docs", response_class=HTMLResponse)
-def docs():
-    html_content = """
-    <html>
-    <body>
-        <p>"Даров!"</p>
-    </body>
-    </html>
-    """
-    return Response(content = html_content, status_code = HTTPStatus.OK)
